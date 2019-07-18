a64d28e741d96b4e233ab05f0b1a541a6a15bf0d
Shuta Fujii <shuuutaf@gmail.com>
2019-07-18 12:05:36 +0000

    First Commit

---
+++ dns_setup/dns_setup.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+if [ ! -f "${SCRIPT_DIR}/conf.txt" ]; then
+	echo "There is no conf.txt file."
+	exit 1
+fi
+
+. ${SCRIPT_DIR}/conf.txt
+
+
+dns_records=()
+
+while read line; do
+	dns_records+=("$line")
+done << END
+`gcloud dns record-sets list --zone=${DNS_ZONE}`
+END
+
+gcloud dns record-sets transaction start --zone ${DNS_ZONE}
+
+for i in `seq 0 ${#dns_records[@]}`; do
+	if [ $i -eq 0 -o $i -eq ${#dns_records[@]} ]; then
+		continue
+	fi
+
+	set ${dns_records[${i}]}
+	name=${1}
+	type=${2}
+	ttl=${3}
+	data=${4}
+
+	if [ ${type} != "A" ]; then
+		continue
+	fi
+
+	#echo "[name]${name} [type]${type} [ttl]${ttl} [data]${data}"
+
+	gcloud dns record-sets transaction remove --zone ${DNS_ZONE} \
+		--name ${name} --ttl ${ttl} \
+		--type ${type} "${data}"
+
+done
+
+
+
+instances=()
+
+while read line; do
+	instances+=("$line")
+done << END
+`gcloud compute instances list`
+END
+
+for i in `seq 0 ${#instances[@]}`; do
+	if [ $i -eq 0 -o $i -eq ${#instances[@]} ]; then
+		continue
+	fi
+
+	set ${instances[${i}]}
+	name=${1}
+	internal=${4}
+	external=${5}
+	status=${6}
+	#echo "[name]${name} [internal]${internal} [external]${external} [status]${statns}"
+
+	gcloud dns record-sets transaction add "${internal}" \
+		--name ${name}.i.${DOMAIN} \
+		--ttl ${TTL} --type A \
+		--zone ${DNS_ZONE}
+	gcloud dns record-sets transaction add "${external}" \
+		--name ${name}.e.${DOMAIN} \
+		--ttl ${TTL} --type A \
+		--zone ${DNS_ZONE}
+
+done
+
+gcloud dns record-sets transaction execute --zone ${DNS_ZONE}
+
+
+gcloud dns record-sets list --zone=${DNS_ZONE}
+
---
+++ gce_setup/instance_install.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+curl https://krypt.co/kr | sh
+
+sudo apt update &&\
+sudo apt install -y gnupg2 ca-certificates lsb-release \
+    apt-transport-https software-properties-common
+
+sudo apt install -y git tmux mosh
+
+
+echo "deb http://nginx.org/packages/debian `lsb_release -cs` nginx" \
+    | sudo tee /etc/apt/sources.list.d/nginx.list
+curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo apt-key add -
+
+sudo apt update &&\
+sudo apt install -y nginx
+
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt update &&\
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+
+sudo apt install -y  locales
+sudo echo "ja_JP.UTF-8 UTF-8" > /etc/locale.gen
+sudo locale-gen ja_JP.UTF-8
+sudo localedef -i ja_JP -c -f UTF-8 -A /usr/share/locale/locale.alias ja_JP.UTF-8
+sudo /usr/sbin/update-locale LANG=ja_JP.UTF-8
+
+
+sudo mkdir -p /usr/local/etc/bash_completion.d
+sudo wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -O /usr/local/etc/bash_completion.d/git-completion.bash
+sudo chmod a+x  /usr/local/etc/bash_completion.d/git-completion.bash
+sudo wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -O /usr/local/etc/bash_completion.d/git-prompt.sh
+sudo chmod a+x /usr/local/etc/bash_completion.d/git-prompt.sh
+
