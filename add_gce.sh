624134628b041067781eef32d5a5d5277b6c4fa0
Shuta Fujii <shuuutaf@gmail.com>
2019-07-19 22:05:48 +0900

    GCEのインスタンスを作成するスクリプトを追加

---
+++ add_gce.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+CONF_FILE="${SCRIPT_DIR}/conf.txt"
+
+if [ ! -f "${CONF_FILE}" ]; then
+	echo "There is no conf.txt file."
+	exit 1
+fi
+
+. ${CONF_FILE}
+
+
+
+INSTANCE_NAME="$1"
+
+if [ "${INSTANCE_NAME}" = "" ]; then
+	echo "Instance name required." 1>&2
+	exit 1
+fi
+
+gcloud compute --project "${PROJECT_NAME}" \
+	instances create "${INSTANCE_NAME}" \
+	--zone "asia-northeast1-b" \
+	--machine-type "g1-small" \
+	--image-family "debian-9" \
+	--image-project "debian-cloud" \
+	--metadata-from-file startup-script="${SCRIPT_DIR}/gce_setup/instance_install.sh"
+
+
+sh ${SCRIPT_DIR}/dns_setup/dns_setup.sh
+
