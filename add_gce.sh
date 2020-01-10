56e31fc964f31384dc8579953ef27c01535f2386
Shuta Fujii <shuuutaf@gmail.com>
2020-01-10 09:21:09 +0000

    インスタンスの設定をファイルに分離

--- add_gce.sh
+++ add_gce.sh
@@ -12,6 +12,14 @@ fi
 . ${CONF_FILE}
 
 
+INSTANCE_DEFAULT_CONF="${SCRIPT_DIR}/instance_conf.txt"
+INSTANCE_CONF=${INSTANCE_DEFAULT_CONF}
+
+INSTANCE_ZONE="asia-northeast1-b"
+INSTANCE_MACHINE_TYPE="g1-small"
+INSTANCE_IMAGE_FAMILY="debian-10"
+INSTANCE_IMAGE_PROJECT="debian-cloud"
+INSTANCE_PREEMPTIBLE=""
 
 INSTANCE_NAME="$1"
 
@@ -20,16 +28,76 @@ if [ "${INSTANCE_NAME}" = "" ]; then
 	exit 1
 fi
 
-gcloud compute --project "${PROJECT_NAME}" \
-	instances create "${INSTANCE_NAME}" \
-	--zone "asia-northeast1-b" \
-	--machine-type "g1-small" \
-	--image-family "debian-10" \
-	--image-project "debian-cloud" \
-	--metadata-from-file startup-script="${SCRIPT_DIR}/gce_setup/instance_install.sh"
+if [ ! "${2}" = "" ]; then
+	echo "Change file setting to ${2}"
+	INSTANCE_CONF=${2}
+fi
 
 
-echo -e '\nDNS setup start.\n'
+if [ ! -f "${INSTANCE_CONF}" ]; then
+	echo "There is no instance config file."
+	echo "${INSTANCE_CONF}"
+	echo "Creating Instance with default settings."
+else
+	echo "Creating Instance with ${INSTANCE_CONF} settings."
+	IFS=$'\n'
+	INSTANCE_DATA=(`cat "$INSTANCE_CONF"`)
+	for i in ${INSTANCE_DATA[@]}; do
+		case "${i%%=*}" in
+			"zone" )
+				if [ "$i" = "zone" ];then
+					continue
+				fi
+				INSTANCE_ZONE="${i#*=}"
+				;;
+			"machine-type" )
+				if [ "$i" = "machine-type" ];then
+					continue
+				fi
+				INSTANCE_MACHINE_TYPE="${i#*=}"
+				;;
+			"image-family" )
+				if [ "$i" = "image-family" ];then
+					continue
+				fi
+				INSTANCE_IMAGE_FAMILY="${i#*=}"
+				;;
+			"image-project" )
+				if [ "$i" = "image-project" ];then
+					continue
+				fi
+				INSTANCE_IMAGE_PROGECT="${i#*=}"
+				;;
+			"preemptible" )
+				INSTANCE_PREEMPTIBLE="--preemptible"
+				;;
+		esac
+	done
+fi
 
-${SCRIPT_DIR}/dns_setup/dns_setup.sh
+echo ""
+echo "Project Name:  ${PROJECT_NAME//\"}"
+echo "Instance Name: ${INSTANCE_NAME//\"}"
+echo "Zone:          ${INSTANCE_ZONE//\"}"
+echo "Machine Type:  ${INSTANCE_MACHINE_TYPE//\"}"
+echo "Image Family:  ${INSTANCE_IMAGE_FAMILY//\"}"
+echo "Image Project: ${INSTANCE_IMAGE_PROJECT//\"}"
+if [ "${INSTANCE_PREEMPTIBLE}" = "" ]; then
+	echo 'Preemptible:   false'
+else
+	echo 'Preemptible:   true'
+fi
+
+#gcloud compute --project "${PROJECT_NAME}" \
+#	instances create "${INSTANCE_NAME}" \
+#	--zone ${INSTANCE_ZONE} \
+#	--machine-type ${INSTANCE_MACHINE_TYPE} \
+#	--image-family ${INSTANCE_IMAGE_FAMILY} \
+#	--image-project ${INSTANCE_IMAGE_PROJECT} \
+#	--metadata-from-file startup-script="${SCRIPT_DIR}/gce_setup/instance_install.sh" \
+#	${INSTANCE_PREEMPTIBLE}
+
+#echo -e '\nDNS setup start.\n'
+#
+#${SCRIPT_DIR}/dns_setup/dns_setup.sh
 
